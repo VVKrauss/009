c7321c326a9586146cf89aad4cd94dfab9c8b952
VVKrauss <112692988+VVKrauss@users.noreply.github.com>
2025-06-05 23:46:48 +0200

    Updated EventsList.tsx

--- src/components/events/EventsList.tsx
+++ src/components/events/EventsList.tsx
@@ -33,7 +33,7 @@ type EventsListProps = {
 const EVENT_TYPE_MAP: Record<string, string> = {
   'lecture': 'Лекция',
   'workshop': 'Мастер-класс',
-  'movie Discussion': 'Обсуждение фильма',
+  'movie discussion': 'Обсуждение фильма',
   'Conversation Club': 'Разговорный клуб',
   'Festival': 'Фестиваль',
   'Stand-up': 'Стендап',
