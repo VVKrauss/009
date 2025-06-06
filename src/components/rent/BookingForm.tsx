ebb66fe5dbea430713a63b0f159c0258f5c91ec7
VVKrauss <112692988+VVKrauss@users.noreply.github.com>
2025-06-06 02:11:49 +0200

    Updated BookingForm.tsx

--- src/components/rent/BookingForm.tsx
+++ src/components/rent/BookingForm.tsx
@@ -91,8 +91,6 @@ const BookingForm = () => {
   useEffect(() => {
     fetchTimeSlots();
 
-    
-    return () => clearTimeout(timer);
   }, []);
 
   const showErrorToast = (message: React.ReactNode) => {
