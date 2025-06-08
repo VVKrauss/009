6a6729a50ef94d8c93b098c7e97071d8ba305341
VVKrauss <112692988+VVKrauss@users.noreply.github.com>
2025-06-08 22:13:23 +0200

    Updated BookingForm.tsx

--- src/components/rent/BookingForm.tsx
+++ src/components/rent/BookingForm.tsx
@@ -327,7 +327,7 @@ const BookingForm = () => {
       
        await sendTelegramNotificationWithFeedback("import.meta.env.VITE_TELEGRAM_CHAT_ID", message);
       
-      );
+
     } catch (err) {
       console.error('Ошибка отправки в Telegram:', err);
     }
